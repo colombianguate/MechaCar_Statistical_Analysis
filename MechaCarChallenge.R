fb6c4108c2ebc0f6ed5b4a9453c593c088d5e5a7
colombianguate <50925704+colombianguate@users.noreply.github.com>
2021-01-16 21:33:03 -0800

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,4 @@ library(dplyr)
 mpg_data <- read.csv('MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_data)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_data)) #generate summary statistics
+
