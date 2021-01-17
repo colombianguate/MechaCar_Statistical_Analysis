517c3673360edc764fa6642c5e1fcb66e51fb4b1
colombianguate <50925704+colombianguate@users.noreply.github.com>
2021-01-16 21:36:29 -0800

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,6 @@ mpg_data <- read.csv('MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_data)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_data)) #generate summary statistics
 
+sus_coil <- read.csv('Suspension_Coil.csv')
+
+total_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups =  'keep' )
